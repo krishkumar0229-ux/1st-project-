6c52fd3e0f4257e397fb61297aeb454510ea928e
Krish Kumar Gupta <krishkumar0229@gmail.com>
2026-07-22 00:00:44 +0530

    Add files via upload

--- PlatformGame.java
+++ PlatformGame.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import javax.sound.sampled.*;
+import java.io.*;
 public class PlatformGame {
 
     public static void main(String[] args) {
@@ -22,8 +24,14 @@ class GamePanel extends JPanel implements Runnable {
     final int SCREEN_HEIGHT = 600;
     Thread gameThread;
     KeyHandler keyH = new KeyHandler();
+    Sound sound = new Sound();
     Player player = new Player(100,100);
+    int distanceScore = 0;
+    int coinScore = 0;
+    int score = 0;
+    int highScore = 0;  
     ArrayList<Platform> platforms = new ArrayList<>();
+    ArrayList<Coin> coins = new ArrayList<>();
     int cameraX = 0;
 
     public GamePanel(){
@@ -33,6 +41,7 @@ class GamePanel extends JPanel implements Runnable {
         setFocusable(true);
         addKeyListener(keyH);
         createWorld();
+        loadHighScore();
 
     }
 
@@ -71,25 +80,110 @@ class GamePanel extends JPanel implements Runnable {
         platforms.add(new Platform(4350,370,120,20));
         platforms.add(new Platform(4550,300,120,20));
         platforms.add(new Platform(4750,240,120,20));
+        coins.add(new Coin(280,420));
+
+        coins.add(new Coin(530,300));
+
+        coins.add(new Coin(780,200));
+
+        coins.add(new Coin(1080,370));
+
+        coins.add(new Coin(1380,270));
+
+        coins.add(new Coin(1780,340));
+
+        coins.add(new Coin(2180,210));
+
+        coins.add(new Coin(2580,390));
+
+        coins.add(new Coin(3180,310));
+
+        coins.add(new Coin(3780,180));
     }
 
     public void startGameThread(){
         gameThread=new Thread(this);
         gameThread.start();
+    }
+    public void loadHighScore() {
+
+    try {
+
+        File file = new File("highscore.txt");
+
+        if (!file.exists()) {
+
+            file.createNewFile();
+            return;
+
+        }
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String line = br.readLine();
+
+        if (line != null) {
+
+            highScore = Integer.parseInt(line);
+
+        }
+
+        br.close();
+
+    } catch (Exception e) {
+
+        System.out.println("Unable to load high score.");
 
     }
 
+}
+    public void saveHighScore() {
+
+    try {
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("highscore.txt"));
+
+        bw.write(String.valueOf(highScore));
+
+        bw.close();
+
+    } catch (Exception e) {
+
+        System.out.println("Unable to save high score.");
+
+    }
+
+}
     @Override
     public void run(){
-
         while(gameThread!=null){
-
             player.update(keyH,platforms);
+            for(Coin c : coins){
+    if(!c.collected){
+        Rectangle playerRect =
+                new Rectangle(player.x,player.y,
+                        player.width,player.height);
+        Rectangle coinRect =
+                new Rectangle(c.x,c.y,c.size,c.size);
 
+        if(playerRect.intersects(coinRect)){
+            c.collected = true;
+            coinScore += 10;
+            sound.play("coin.wav");
+        }
+
+    }
+
+}
+           distanceScore = Math.max(distanceScore, player.x / 10);
+           score = distanceScore + coinScore; 
+           if (score > highScore) {
+           highScore = score;
+           saveHighScore();
+
+}
             cameraX=player.x-250;
-
             repaint();
-
             try{
 
                 Thread.sleep(16);
@@ -114,11 +208,16 @@ class GamePanel extends JPanel implements Runnable {
         g2.setColor(new Color(135,206,235));
         g2.fillRect(cameraX,0,SCREEN_WIDTH,SCREEN_HEIGHT);
         // Platforms //
-        for(Platform p:platforms){
+      
+        for (Platform p : platforms) {
+           p.draw(g2);
+}
 
-            p.draw(g2);
+        // Coins //
+        for (Coin c : coins) {
+         c.draw(g2);
+}
 
-        }
         // Player //
         player.draw(g2);
 
@@ -130,11 +229,15 @@ class GamePanel extends JPanel implements Runnable {
         g2.setFont(new Font("Arial",Font.BOLD,20));
         g2.drawString("Arrow Keys = Move",20,30);
         g2.drawString("Space = Jump",20,60);
+        g2.drawString("Score : " + score,20,90);
+        g2.drawString("High Score : " + highScore,20,120);
         if(player.gameOver){
             g2.setFont(new Font("Arial",Font.BOLD,50));
             g2.setColor(Color.RED);
             g2.drawString("GAME OVER",240,250);
-
+            g2.setFont(new Font("Arial", Font.BOLD, 25));
+            g2.setColor(Color.BLACK);
+            g2.drawString("Final Score : " + score, 280, 300);
         }
 
     }
@@ -147,7 +250,7 @@ class Player {
     int height = 50;
     double velocityX = 0;
     double velocityY = 0;
-
+    
     // Normal values that we have in our Physics //
     final double gravity = 0.5;
     final double friction = 0.90;
@@ -156,7 +259,7 @@ class Player {
     final double jumpForce = -15;
     boolean onGround = false;
     boolean gameOver = false;
-
+    Sound sound = new Sound();
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
@@ -184,10 +287,11 @@ class Player {
 
         // To Jump //
         if (keyH.jumpPressed && onGround) {
-            velocityY = jumpForce;
-            onGround = false;
+           velocityY = jumpForce;
+           onGround = false;
+           sound.play("jump.wav");
         }
-
+        
         // Gravity //
         velocityY += gravity;
 
@@ -248,11 +352,12 @@ class Player {
         }
 
         // Fall below world //
-        if (y > 700) {
-            gameOver = true;
-        }
+        if (y > 700 && !gameOver) {
+        gameOver = true;
+        sound.play("gameover.wav");
 
-    }
+}
+}
 
     public void draw(Graphics2D g2) {
 
@@ -352,4 +457,38 @@ class Collision {
 
     }
 
+}
+class Coin {
+    int x;
+    int y;
+    int size = 20;
+    boolean collected = false;
+    public Coin(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+    public void draw(Graphics2D g2){
+        if(collected)
+            return;
+
+        g2.setColor(Color.YELLOW);
+        g2.fillOval(x,y,size,size);
+        g2.setColor(Color.ORANGE);
+        g2.drawOval(x,y,size,size);
+
+    }
+
+}
+class Sound {
+    public void play(String soundFile) {
+        try {
+            AudioInputStream audio =
+                    AudioSystem.getAudioInputStream(new File(soundFile));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("Couldn't play: " + soundFile);
+        }
+    }
 }
